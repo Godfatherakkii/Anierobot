652566aa0c004b72df08a969e762570410e67f1e
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-20 18:54:26 +0530

    Update cust_filters_sql.py

--- AnieRobot/Modules/sql/cust_filters_sql.py
+++ AnieRobot/Modules/sql/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
-from marvel.modules.helper_funcs.msg_types import Types
-from marvel.modules.sql import BASE, SESSION
+from AnieRobot.modules.helper_funcs.msg_types import Types
+from AnieRobot.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
