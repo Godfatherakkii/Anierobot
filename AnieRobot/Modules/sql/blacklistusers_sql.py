0e7f8d8155354a2c559fc2eef46699889b1ed63c
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-20 18:51:36 +0530

    Update blacklistusers_sql.py

--- AnieRobot/Modules/sql/blacklistusers_sql.py
+++ AnieRobot/Modules/sql/blacklistusers_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText
 
-from marvel.modules.sql import BASE, SESSION
+from AnieRobot.modules.sql import BASE, SESSION
 
 
 class BlacklistUsers(BASE):
