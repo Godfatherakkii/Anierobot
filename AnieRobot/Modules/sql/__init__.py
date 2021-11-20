4e6a47f240244be4c5a50b443b761abd29da8f18
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-20 18:45:06 +0530

    Update __init__.py

--- AnieRobot/Modules/sql/__init__.py
+++ AnieRobot/Modules/sql/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-from marvel import DB_URI
+from AnieRobot import DB_URI
 
 
 def start() -> scoped_session:
