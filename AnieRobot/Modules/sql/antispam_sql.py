db413da7fdc37f09d27e29a9283471e7b5dc582c
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-20 18:48:10 +0530

    Update antispam_sql.py

--- AnieRobot/Modules/sql/antispam_sql.py
+++ AnieRobot/Modules/sql/antispam_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Integer, String, Boolean
 
-from marvel.modules.sql import BASE, SESSION
+from AnieRobot.modules.sql import BASE, SESSION
 
 
 class GloballyBannedUsers(BASE):
