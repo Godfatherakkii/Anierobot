04037987318315de33541951a31304370f754344
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-20 18:45:48 +0530

    Update admin_sql.py

--- AnieRobot/Modules/sql/admin_sql.py
+++ AnieRobot/Modules/sql/admin_sql.py
@@ -3,7 +3,7 @@ from typing import Union
 
 from sqlalchemy import Column, Integer, String, Boolean
 
-from marvel.modules.sql import SESSION, BASE
+from AnieRobot.modules.sql import SESSION, BASE
 
 
 class CommandReactionChatSettings(BASE):
