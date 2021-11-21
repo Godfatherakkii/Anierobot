d5f42073ebf0ffa4243b385db3fcc3145d0c7fe0
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-21 11:24:44 +0530

    Update rss_sql.py

--- AnieRobot/Modules/sql/rss_sql.py
+++ AnieRobot/Modules/sql/rss_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Integer
 
-from marvel.modules.sql import BASE, SESSION
+from AnieRobot.modules.sql import BASE, SESSION
 
 
 class RSS(BASE):
