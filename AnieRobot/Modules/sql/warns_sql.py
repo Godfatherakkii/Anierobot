bd229fc0640664159edefcc2fc373340291f3ee7
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-21 12:19:28 +0530

    Update warns_sql.py

--- AnieRobot/Modules/sql/warns_sql.py
+++ AnieRobot/Modules/sql/warns_sql.py
@@ -3,7 +3,7 @@ import threading
 from sqlalchemy import Integer, Column, String, UnicodeText, func, distinct, Boolean
 from sqlalchemy.dialects import postgresql
 
-from marvel.modules.sql import SESSION, BASE
+from AnieRobot.modules.sql import SESSION, BASE
 
 
 class Warns(BASE):
