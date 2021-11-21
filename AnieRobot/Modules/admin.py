45e76d454894898cd8d847562e21d0b027b1b47e
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-21 12:23:16 +0530

    Update admin.py

--- AnieRobot/Modules/admin.py
+++ AnieRobot/Modules/admin.py
@@ -1,4 +1,4 @@
-#Copyright (C) 2021 Free Software @noobanon @FakeMasked , Inc.[ https://t.me/noobanon https://t.me/FakeMasked ]
+#Copyright (C) 2021 Free Software @d3nvil @FakeMasked , Inc.[ https://t.me/d3nvil https://t.me/FakeMasked ]
 #Everyone is permitted to copy and distribute verbatim copies
 #of this license document, but changing it is not allowed.
 #The GNGeneral Public License is a free, copyleft license for
@@ -15,15 +15,15 @@ from telegram.ext import CommandHandler, Filters
 from telegram.ext.dispatcher import run_async
 from telegram.utils.helpers import escape_markdown, mention_html
 
-from marvel import dispatcher, updater
-from marvel.modules.disable import DisableAbleCommandHandler
-from marvel.modules.helper_funcs.chat_status import bot_admin, can_promote, user_admin, can_pin,  user_can_promote, user_can_pin
-from marvel.modules.helper_funcs.extraction import extract_user
-from marvel.modules.log_channel import loggable
-from marvel.modules.sql import admin_sql as sql
-from marvel.modules.translations.strings import tld
+from AnieRobot import dispatcher, updater
+from AnieRobot.modules.disable import DisableAbleCommandHandler
+from AnieRobot.modules.helper_funcs.chat_status import bot_admin, can_promote, user_admin, can_pin,  user_can_promote, user_can_pin
+from AnieRobot.modules.helper_funcs.extraction import extract_user
+from AnieRobot.modules.log_channel import loggable
+from AnieRobot.modules.sql import admin_sql as sql
+from AnieRobot.modules.translations.strings import tld
 
-from marvel.modules.connection import connected
+from AnieRobot.modules.connection import connected
 
 
 @bot_admin
