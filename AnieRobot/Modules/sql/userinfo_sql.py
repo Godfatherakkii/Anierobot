9a5083b1f0c94743141037248b950e7c9a09fd85
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-21 12:18:26 +0530

    Update userinfo_sql.py

--- AnieRobot/Modules/sql/userinfo_sql.py
+++ AnieRobot/Modules/sql/userinfo_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, Integer, UnicodeText
 
-from marvel.modules.sql import SESSION, BASE
+from AnieRobot.modules.sql import SESSION, BASE
 
 
 class UserInfo(BASE):
