9facac64fd0007415c463b78416dd3aae3205dcc
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-21 09:47:26 +0530

    Update locks_sql.py

--- AnieRobot/Modules/sql/locks_sql.py
+++ AnieRobot/Modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from marvel.modules.sql import SESSION, BASE
+from AnieRobot.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
