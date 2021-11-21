14b55259be6d598385c9843f59f1b1d7a82f55df
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-21 20:15:48 +0530

    Update reporting.py

--- AnieRobot/Modules/reporting.py
+++ AnieRobot/Modules/reporting.py
@@ -13,10 +13,10 @@ from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CommandHandler, RegexHandler, run_async, Filters, CallbackQueryHandler
 from telegram.utils.helpers import mention_html
 
-from marvel import dispatcher, LOGGER
-from marvel.modules.helper_funcs.chat_status import user_not_admin, user_admin
-from marvel.modules.log_channel import loggable
-from marvel.modules.sql import reporting_sql as sql
+from AnieRobot import dispatcher, LOGGER
+from AnieRobot.modules.helper_funcs.chat_status import user_not_admin, user_admin
+from AnieRobot.modules.log_channel import loggable
+from AnieRobot.modules.sql import reporting_sql as sql
 
 REPORT_GROUP = 5
 
