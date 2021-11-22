e9b5279ef3885df062e38789b5c966717bbd89bb
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-22 12:37:20 +0530

    Update log_channel_sql.py

--- AnieRobot/modules/sql/log_channel_sql.py
+++ AnieRobot/modules/sql/log_channel_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, func, distinct
 
-from marvel.modules.sql import BASE, SESSION
+from AnieRobot.modules.sql import BASE, SESSION
 
 
 class GroupLogs(BASE):
