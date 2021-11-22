7c130878f39656318b474c76c2169e8bbb9066ff
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-22 22:38:16 +0530

    Update __main__.py

--- AnieRobot/__main__.py
+++ AnieRobot/__main__.py
@@ -808,7 +808,7 @@ def main():
     settings_handler = CommandHandler("settings", get_settings)
     settings_callback_handler = CallbackQueryHandler(settings_button, pattern=r"stngs_")
 
-    about_callback_handler = CallbackQueryHandler(anie_about_callback, pattern=r"anie_")
+    about_callback_handler = CallbackQueryHandler(innexia_about_callback, pattern=r"anie_")
     source_callback_handler = CallbackQueryHandler(Source_about_callback, pattern=r"source_")
 
     donate_handler = CommandHandler("donate", donate)
