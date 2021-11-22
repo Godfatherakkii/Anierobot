085fb8207c5240a138d9e5cac4b150a84bedbf09
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-22 17:59:21 +0530

    Update wiki.py

--- AnieRobot/modules/wiki.py
+++ AnieRobot/modules/wiki.py
@@ -31,9 +31,5 @@ def wiki(update, context):
     summary = '{} <a href="{}">more</a>'
     update.message.reply_text(summary.format(wikipedia.summary(reply, sentences=3), wikipedia.page(reply).url))
 		
-__help__ = """
- - /wiki text: Returns search from wikipedia for the input text
-"""
-__mod_name__ = "WikiPedia"
 WIKI_HANDLER = DisableAbleCommandHandler("wiki", wiki, pass_args=True)
 dispatcher.add_handler(WIKI_HANDLER)
